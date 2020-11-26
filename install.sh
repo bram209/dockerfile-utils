037966a58d1eb7971b8d5108fc3244affe60dac2
Bram <bram.hoendervangers@gmail.com>
2020-11-26 15:00:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-read -p "Do you wish to see the content of this script before execution?" -n 1 -r
-
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  tail -n +20 "$0"
-
-  echo
-  read -p "Continue? " -n 1 -r
-  echo
-
-  if [[ $REPLY =~ ^[Nn]$ ]]
-  then
-    exit
-  fi
-fi
-
-echo "INSTALL"
+echo "INSTALLING"
 curl -sS https://raw.githubusercontent.com/bram209/dockerfile-utils/main/gen_aspnet_dockerfile.sh | sudo tee -a /usr/local/bin/gen_aspnet_dockerfile &>/dev/null && echo "Installed gen_aspnet_dockerfile into /usr/local/bin"
