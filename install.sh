719bc697b9bb9f2c15721eee26f175414b5d6c4d
Bram <bram.hoendervangers@gmail.com>
2020-11-26 15:02:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "INSTALLING"
-curl -sS https://raw.githubusercontent.com/bram209/dockerfile-utils/main/gen_aspnet_dockerfile.sh | sudo tee -a /usr/local/bin/gen_aspnet_dockerfile &>/dev/null && echo "Installed gen_aspnet_dockerfile into /usr/local/bin"
+curl -sS https://raw.githubusercontent.com/bram209/dockerfile-utils/main/gen_aspnet_dockerfile.sh | sudo tee -a /usr/local/bin/gen_aspnet_dockerfile &>/dev/null && sudo chmod +x /usr/local/bin/gen_aspnet_dockerfile  && echo "Installed gen_aspnet_dockerfile into /usr/local/bin"
